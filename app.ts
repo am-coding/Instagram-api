a493321f6bae7a762380bcd8d7bcac4d4101c121
amrik malhans <amrikdeveloper@gmail.com>
2020-07-26 18:19:26 +0530

    initial commit

--- app.ts
+++ app.ts
@@ -1,20 +1,21 @@
+import { IgApiClientRealtime, withRealtime } from 'instagram_mqtt';
 import { DirectThreadEntity, IgApiClient } from 'instagram-private-api';
-
+import dotenv from "dotenv";
 const ig = new IgApiClient();
+
+dotenv.config()
 
 async function login() {
   // basic login-procedure
-  ig.state.generateDevice(process.env.YOUR_USERNAME);
-  await ig.account.login(process.env.YOUR_USERNAME, process.env.PASSWORD);
+  ig.state.generateDevice(process.env.IG_USERNAME);
+  await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 }
 
 (async () => {
   await login();
-  const userId = await ig.user.getIdByUsername('username'); // enter username you wanna send text to
+
+const userId = await ig.user.getIdByUsername('username');
 const thread = ig.entity.directThread([userId.toString()]);
+await thread.broadcastText('Message from node');
 
-  const element  = thread.broadcastText('enter text/message');  
-  await element;
-
-  
 })();
